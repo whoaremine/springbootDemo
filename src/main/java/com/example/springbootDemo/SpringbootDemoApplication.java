752d4818c859526fa0cae21435898a0eeba60cb4
whoaremine <953422128@qq.com>
2020-08-22 15:34:32 +0800

    add some base  configuration

--- src/main/java/com/example/springbootDemo/SpringbootDemoApplication.java
+++ src/main/java/com/example/springbootDemo/SpringbootDemoApplication.java
@@ -2,8 +2,13 @@ package com.example.springbootDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class})
 public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
