20dfd81f5ceddd98c69f9ff24af4be455f47ccfa
whoaremine <953422128@qq.com>
2021-01-18 20:21:22 +0800

    add test

--- src/test/java/com/example/springbootDemo/SpringbootDemoApplicationTests.java
+++ src/test/java/com/example/springbootDemo/SpringbootDemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.springbootDemo;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 
 @SpringBootTest
 class SpringbootDemoApplicationTests {
-
 	@Test
 	void contextLoads() {
+		System.out.println("how are you");
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		open("https://google.com/ncr");
+
+		$(By.name("q")).val("selenide").pressEnter();
+//		new GooglePage().searchFor("selenide");
+//
+//		SearchResultsPage results = new SearchResultsPage();
+//		results.getResults().shouldHave(sizeGreaterThan(1));
+//		results.getResult(0).shouldHave(text("Selenide: concise UI tests in Java"));
+
 	}
 
 }
